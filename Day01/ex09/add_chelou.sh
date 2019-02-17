9074f73d8f0e2e4d59ccaab6687401806040e712
mescande <matthieu.escande@gmail.com>
2019-02-17 18:27:07 +0100

    Day01 end

--- Day01/ex09/add_chelou.sh
+++ Day01/ex09/add_chelou.sh
@@ -1,1 +1,1 @@
-echo 'obase=13;ibase=5;'"$FT_NBR1+$FT_NBR2" | tr "mrdoc" "01234" | tr "'\"?!" "0234" | tr  "\\" "1" | sed 's/3/o/' | bc | tr "0123456789ABC" "gtaio luSnemf"
+echo "obase=13;ibase=5;$FT_NBR1+$FT_NBR2" | tr "mrdoc" "01234" | tr "'\"?!" "0234" | tr  "\\" "1" | sed 's/3/o/' | bc | tr "0123456789ABC" "gtaio luSnemf"
